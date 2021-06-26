04b2a2adc1d396585e63bbe98f4a7fd07c740ca2
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-26 18:45:36 -0400

    Added functionality for css file transpiling and extracted caching logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -9,22 +9,55 @@ const fileCache = localForage.createInstance({
 export const fetchPlugin = (inputCode: string) => {
     return {
         name: 'fetch-plugin',
-        setup(build: esbuild.PluginBuild) {     
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                // We load up the contents for ESbuild
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode,
-                    };
+        setup(build: esbuild.PluginBuild) {   
+            
+            // Loads up the contents for ESbuild
+            build.onLoad({ filter: /(^index\.js$)/ }, () => {
+                return {
+                    loader: 'jsx',
+                    contents: inputCode,
                 };
+            });
 
+            // Middleware loader for cache checks
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 // Checks if the bundle has already been fetched
                 const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
                 if (cachedResult) {
                     return cachedResult;
                 };
+            });
 
+            // Loads up CSS files
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+                const { data, request } = await axios.get(args.path);
+
+                // Fixes any possible characters that break template string
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replacee(/"/g, '\\"')
+                    .replace(/'/g, "\\'");
+
+                // Appends style tag to head of document
+                const contents = `
+                    const style = document.createElement('style);
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                `;
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL ('./', request.responseURL).pathname,
+                };
+
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
+            
+            // Loads up JSX files
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 const { data, request } = await axios.get(args.path);
 
                 const result: esbuild.OnLoadResult = {
