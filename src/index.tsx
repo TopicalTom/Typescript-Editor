6bb93378f5b7a22389d5602832cd350064c1169f
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-27 04:50:30 -0400

    Created Text editor ui and functionality

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDom from 'react-dom';
 
 // Components
-import CodeCell from './components/CodeCell';
+//import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 
 const App = () => {
     return (
         <div>
-            <CodeCell />
+            <TextEditor />
         </div>
     );
 };
